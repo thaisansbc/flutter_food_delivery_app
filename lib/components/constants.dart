2fccf939f1f7884fa4a00a76bcae2139b8d9619f
unknown <thaisan.sbc@gmail.com>
2023-01-08 15:32:49 +0700

    first push

---
+++ lib/components/constants.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const Color PrimaryColor = Color(0xFF843985);
+const Color SecondaryColor = Color(0xFF843985);
+const Color ThirdColor = Color(0xFF843985);
+const Color TertiaryColor = Color(0xFF843985);
+const Color QuaternaryColor = Color(0xFF843985);
